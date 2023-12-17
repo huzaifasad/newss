a35df3cca9d4e48fd4a673303d04c6f3c28fc7d1
Huzaifa Saad <140094374+huzaifasad@users.noreply.github.com>
2023-12-17 12:21:29 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
+import laptoprouter = from './schemas/productrouter/laptop';
+import userrouter = from './schemas/productrouter/user';
 const app = express();
 // index.js (server)
-const url = 'mongodb+srv://mhuzaifatariq7:luckynumber7@cluster0.mjqk6et.mongodb.net/your-database-name?retryWrites=true&w=majority';
+const url = 'mongodb+srv://huzaifa084567:12345@cluster0.wpihnwn.mongodb.net/your-database-name?retryWrites=true&w=majority';
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('connected to the database'))
 .catch(()=> console.log('not conncted'));
@@ -19,8 +21,10 @@ app.use(cors(
 
 ));
 app.get("/",(req,res)=>{
-    res.json("ALLAH");
+    res.json("ALLAH HU AKBAR");
 })
+app.use("/laptop", laptoprouter);
+app.use("/user", userrouter);
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", (req, res) => {
