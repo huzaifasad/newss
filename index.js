516ccab0add4e353599cba50b61795f9a8c11675
Huzaifa Saad <140094374+huzaifasad@users.noreply.github.com>
2023-12-17 12:12:04 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-const express = require("express");
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 const app = express();
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+// index.js (server)
+const url = 'mongodb+srv://mhuzaifatariq7:luckynumber7@cluster0.mjqk6et.mongodb.net/your-database-name?retryWrites=true&w=majority';
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log('connected to the database'))
+.catch(()=> console.log('not conncted'));
 
-const url =
-  "mongodb+srv://huzaifa084567:12345@cluster0.wpihnwn.mongodb.net/your-database-name?retryWrites=true&w=majority";
-mongoose
-  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("connected to the database"))
-  .catch(() => console.log("not conncted"));
+app.listen(5000);
+app.use(cors(
+    {
+        origin:["https://midwork-frontend.vercel.app"],
+        methods:["POST","GET"],
+        credentials:true
+    }
 
-app.listen(1000, () => {
-  console.log("Server is running on port 1000");
-});
-
-app.use(
-  cors()
-);
-const laptoprouter = require("./schemas/productrouter/laptop");
-const userrouter = require("./schemas/productrouter/user");
+));
+app.get("/",(req,res)=>{
+    res.json("Hello");
+})
 app.use(bodyParser.json({ extended: true }));
-app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", (req, res) => {
   res.send("thiis response");
