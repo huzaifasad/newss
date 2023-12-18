f4a130cb1260c873e5b2cb54933203e494841a5d
Huzaifa Saad <140094374+huzaifasad@users.noreply.github.com>
2023-12-18 09:50:38 -0800

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(5000);
 
 app.use(
   cors({
-    origin: ['https://6580423fa6dfa73f5ab104f1--monumental-squirrel-cca672.netlify.app/'],
+    origin: [''],
     methods: ['POST', 'GET', 'PUT', 'DELETE'], // Include PUT and DELETE methods
     credentials: true,
   })
@@ -29,49 +29,45 @@ app.get('/', (req, res) => {
   res.json('ALLAH HU AKBAR');
 });
 
-app.post('/laptop/add', upload.array('photos', 3), async (req, res) => {
-  const { name, price, desc, productQuantity, ramSize, type, brand } = req.body;
-  console.log(req.body);
-  console.log(req.files);
+app.post('/laptop/add', async (req, res) => {
+  try {
+    const { name, price, desc, productQuantity, ramSize, type, brand, features, images } = req.body;
 
-  const images = req.files.map((file) => ({
-    data: file.buffer,
-    contentType: file.mimetype,
-  }));
+    // Create a new laptop instance
+    const newLaptop = new Laptop({
+      name,
+      price,
+      desc,
+      productQuantity,
+      ramSize,
+      type,
+      brand,
+      images,
+      features,
+    });
 
-  const features = JSON.parse(req.body.features);
+    // Save the laptop to the database
+    const savedLaptop = await newLaptop.save();
 
-  const newLaptop = new Laptop({
-    name,
-    price,
-    desc,
-    productQuantity,
-    ramSize,
-    type,
-    brand,
-    images,
-    features,
-  });
-
-  const savedLaptop = await newLaptop.save();
-
-  res.status(201).json({ message: 'Laptop added successfully', laptop: savedLaptop });
+    res.status(201).json({ message: 'Laptop added successfully', laptop: savedLaptop });
+  } catch (error) {
+    console.error('Error adding laptop:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
  
 app.get('/laptop/get', async (req, res) => {
-  try {
+   try {
     const laptops = await Laptop.find();
 
-    const laptopsWithBase64Images = laptops.map((laptop) => {
-      const images = laptop.images.map((image) => ({
-        ...image,
-        data: image.data.toString('base64'),
-      }));
+    // Convert binary image data to base64
+  
 
-      return {
-        ...laptop.toObject(),
-        images,
-      };
+    res.send(laptops);
+  } catch (error) {
+    console.error('Error fetching laptops:', error);
+    res.status(500).send({ error: 'Internal Server Error' });
+  }
     });
 
     res.send(laptopsWithBase64Images);
